58397460b220468adefffb329d4e6c6264cbb8cb
Abel Castillon Jr <ayebosays@gmail.com>
2021-04-23 15:27:41 -0700

    made a simple chat example

--- my-app/index.js
+++ my-app/index.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-/* Route Handler */
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
